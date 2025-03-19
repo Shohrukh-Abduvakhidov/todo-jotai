5b33c26ed2b06e8c6c0bcc4e8829745f8de90fe9
Shohrukh-Abduvakhidov <vaxidov011@gmail.com>
2025-03-19 20:42:09 +0500

    added new todo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { useState, useEffect } from 'react';
 const todosAtom = atom<ITodo[]>([
   { id: '1', title: 'title-1', description: 'description-1', status: false },
   { id: '2', title: 'title-2', description: 'description-2', status: true },
+  { id: '3', title: 'title-3', description: 'description-3', status: false},
+  { id: '4', title: 'title-4', description: 'description-4', status: true },
+  { id: '5', title: 'title-5', description: 'description-5', status: true },
 ]);
 
 interface ITodo {
